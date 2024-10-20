866bc1ba222fb152a2fe16fb2932874d0ef53163
ʕ ᵔᴥᵔ ʔ <email@not.exists>
2024-10-20 23:39:48 +0200

    meow

--- parser/step3/run.py
+++ parser/step3/run.py
@@ -9,17 +9,17 @@ if __name__ == '__main__':
 
     cmd = (fr"cd {base} && .venv\Scripts\activate && py parser/step3/main.py")
 
-    i = 0
+    # i = 0
     while True:
         try:
             print(datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
             subprocess.check_output(cmd, stderr=subprocess.STDOUT, timeout=20, shell=True)
         except subprocess.CalledProcessError as e:
             print('CalledProcessError', e.returncode, e)
-            if i < 3:
-                subprocess.Popen(f'py -c "import ctypes'
-                     f'\nctypes.windll.user32.MessageBoxW(0, \'Check output\','
-                     f' \'Err check output\', 0x1000)"')
-            i += 1
+            # if i < 3:
+            #     subprocess.Popen(f'py -c "import ctypes'
+            #          f'\nctypes.windll.user32.MessageBoxW(0, \'Check output\','
+            #          f' \'Err check output\', 0x1000)"')
+            # i += 1
         time.sleep(10)
         print('Restart')
