2490d9e0a337a51e995b4ac35e84fa41fac1000c
ʕ ᵔᴥᵔ ʔ <email@not.exists>
2024-10-26 21:17:15 +0200

    meow

--- soul_mate/app_catalog/urls.py
+++ soul_mate/app_catalog/urls.py
@@ -6,6 +6,7 @@ view_inst = views.Views()
 urlpatterns = [
     path('', view_inst.index, name='catalog'),
     path('set-s4res/<int:id>/<slug:act>/', view_inst.set_s4res, name='set_s4res'),
+    path('s4good/', view_inst.s4good, name='s4good'),
     path('<path:section_path>/<slug:section_code>/', view_inst.section, name='section'),
     path('<slug:section_code>/', view_inst.section, name='section'),
 ]
--- soul_mate/app_catalog/views.py
+++ soul_mate/app_catalog/views.py
@@ -15,11 +15,13 @@ class Views:
     __S4_RES_CAT_ID_26_MB: int = 55
 
     def index(self, request):
+
         return render(request, 'app_catalog/index.html', {
             'tree': Sections.tree.get()['tree']
         })
 
     def set_s4res(self, request, id: int, act: str):
+
         cat_id = getattr(self, f'_{type(self).__qualname__}__S4_RES_CAT_ID_{self.__CUR_AGE}_{act.upper()}')
         el = Elements.objects.get(pk=id)
         el.sections.add(cat_id)
@@ -30,32 +32,36 @@ class Views:
 
         section_el = get_object_or_404(Sections, code=section_code)
 
-        if section_code == 's3-good':
-            els = Elements.objects.filter(
-                step3_good=True,
-                time_last_active__gte=datetime.fromtimestamp(time.mktime(time.strptime(
-                '1/10/2024', '%d/%m/%Y'))).strftime('%Y-%m-%d %H:%M:%S'),
-                age=self.__CUR_AGE,
-            )
-            els = els.exclude(sections__id__in=(
-                getattr(self, f'_{type(self).__qualname__}__S4_RES_CAT_ID_{self.__CUR_AGE}_YES'),
-                getattr(self, f'_{type(self).__qualname__}__S4_RES_CAT_ID_{self.__CUR_AGE}_NO'),
-                getattr(self, f'_{type(self).__qualname__}__S4_RES_CAT_ID_{self.__CUR_AGE}_MB'),
-            ))
-        else:
-            els = Elements.objects.filter(sections=section_el)
+        pager = Paginator(
+            Elements.objects.filter(sections=section_el).prefetch_related('sections'), 18
+        ).get_page(request.GET.get('page'))
+
+        return render(request, 'app_catalog/section.html', context={
+            'section': section_el,
+            'pager': pager,
+        })
+
+    def s4good(self, request):
+
+        els = Elements.objects.filter(
+            step3_good=True,
+            time_last_active__gte=datetime.fromtimestamp(time.mktime(time.strptime(
+            '1/10/2024', '%d/%m/%Y'))).strftime('%Y-%m-%d %H:%M:%S'),
+            age=self.__CUR_AGE,
+        )
+        els = els.exclude(sections__id__in=(
+            getattr(self, f'_{type(self).__qualname__}__S4_RES_CAT_ID_{self.__CUR_AGE}_YES'),
+            getattr(self, f'_{type(self).__qualname__}__S4_RES_CAT_ID_{self.__CUR_AGE}_NO'),
+            getattr(self, f'_{type(self).__qualname__}__S4_RES_CAT_ID_{self.__CUR_AGE}_MB'),
+        ))
 
         pager = Paginator(
             els.prefetch_related('sections'), 18
         ).get_page(request.GET.get('page'))
 
-        good_count: str = ''
-        if section_code == 's3-good':
-            good_count = f'[{pager.paginator.count}]'
+        good_count: str = f'[{pager.paginator.count}]'
 
         return render(request, 'app_catalog/section.html', context={
-            'tree': Sections.tree.get()['tree'],
-            'section': section_el,
             'pager': pager,
             'good_count': good_count
         })
